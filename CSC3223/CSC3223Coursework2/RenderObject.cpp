26564e219452a84ef43850b4575f9aa2a3db833d
Michael Arnold <marnold0831@hotmail.com>
2019-03-28 11:14:58 +0000

    update 28/03/19

--- CSC3223/CSC3223Coursework2/RenderObject.cpp
+++ CSC3223/CSC3223Coursework2/RenderObject.cpp
@@ -10,14 +10,16 @@ RenderObject::RenderObject(MeshGeometry* mesh, Matrix4 m)
 RenderObject::RenderObject(MeshGeometry* inMesh, const char* varyings, int varyingsSize, bool isTransform, Matrix4 m)
 	: mesh(inMesh), texture(nullptr), shader(nullptr), varyings(varyings), varyingsSize(varyingsSize), isTransform(isTransform), transform(m)
 {
-	glGenBuffers(2, buffer);
-	glBindBuffer(GL_ARRAY_BUFFER, buffer[0]);
-	glBindBuffer(GL_ARRAY_BUFFER, buffer[1]);
 	vector<Vector3> vec = mesh->GetPositionData();
 	Vector3* data = vec.data();
+	glGenBuffers(2, buffer);
+	glBindBuffer(GL_ARRAY_BUFFER, buffer[0]);
+	glBufferData(GL_ARRAY_BUFFER, sizeof(vector<Vector3>) * mesh->GetVertexCount(), data, GL_DYNAMIC_DRAW);
+	glBindBuffer(GL_ARRAY_BUFFER, buffer[1]);
+	glBufferData(GL_ARRAY_BUFFER, sizeof(vector<Vector3>) * mesh->GetVertexCount(), data, GL_DYNAMIC_DRAW);
 	//glBufferData(GL_ARRAY_BUFFER, sizeof(vector<Vector3>) * mesh->GetVertexCount(), data, GL_DYNAMIC_DRAW);
-	glNamedBufferData(buffer[0], sizeof(Vector3) * mesh->GetVertexCount(), data, GL_DYNAMIC_DRAW);
-	glNamedBufferData(buffer[0], sizeof(Vector3) * mesh->GetVertexCount(), data, GL_DYNAMIC_DRAW);
+	//glNamedBufferData(buffer[0], sizeof(Vector3) * mesh->GetVertexCount(), data, GL_DYNAMIC_DRAW);
+	//glNamedBufferData(buffer[0], sizeof(Vector3) * mesh->GetVertexCount(), data, GL_DYNAMIC_DRAW);
 
 	//glGenTransformFeedbacks(2, buffer);
 }
